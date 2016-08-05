ae61b30e9616cf7e5fb21bb0c1fd3e8b31d4f985
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2016-08-05 22:30:57 +0200

    added GameplayServices class (Constants + tag) + empty RoomsManager - only on Youenn's scene so far

---
+++ Assets/Scripts/Gameplay/GameplayService.cs
@@ -0,0 +1,30 @@
+﻿namespace Assets.Scripts.Gameplay
+{
+    public static class GameplayServices
+    {
+        private static IRoomsManager _roomsManager;
+        public static IRoomsManager RoomsManager
+        {
+            get
+            {
+                return _roomsManager ?? (_roomsManager = GetRoomsManager());
+            }
+        }
+
+        // Methods below were made private so programmers only use the properties above!
+        private static IRoomsManager GetRoomsManager()
+        {
+            return new RoomsManager();
+        }
+
+        public class Tags
+        {
+            public const string SomeDefaultTag = "TAG";
+        }
+
+        public class Constants
+        {
+            public const string SomeDefaultConstant = "CONSTANT";
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/GenerateLevel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Gameplay
+{
+    public class GenerateLevel : MonoBehaviour
+    {
+        public GameObject[] Rooms;
+
+        public List<GameObject> GetRooms()
+        {
+            if (Rooms == null || Rooms.Length < 1)
+            {
+                Debug.LogError("[RoomsList] At least one room must be selected!");
+                throw new InvalidOperationException();
+            }
+
+            return Rooms.ToList();
+        }
+    }
+}
