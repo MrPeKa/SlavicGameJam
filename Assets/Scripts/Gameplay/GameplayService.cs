d75bf0712171f496e2e14142c0831f6f496bb26b
MrZacky <lukasz.slowik01@gmail.com>
2016-08-06 10:21:16 +0200

    added sounds for Pikachu and Pamela + test scene

--- Assets/Scripts/Gameplay/GameplayService.cs
+++ Assets/Scripts/Gameplay/GameplayService.cs
@@ -25,6 +25,44 @@
         public class Constants
         {
             public const string SomeDefaultConstant = "CONSTANT";
+
+            public const string INTRO = "Intro";
+            public const string HIT = "Hit";
+            public const string GET_HIT = "GetHit";
+            public const string DEAD = "Dead";
+            public const string FOOTSTEPS = "FootSteps";
+
+
+            private const string SOUNDS_PATH = "Sounds/";
+
+            public const string POKEMON = "Pokemon";
+            public const string BAY_WATCH = "BayWatch";
+            public const string POWER_RANGERS = "PowerRangers";
+            public const string DISCO = "Disco";
+
+            public const string POKEMON_CLIPS_PATH = SOUNDS_PATH + POKEMON + "/";
+            public const string BAY_WATCH_CLIPS_PATH = SOUNDS_PATH + BAY_WATCH + "/";
+            public const string POWER_RANGERS_CLIPS_PATH = SOUNDS_PATH + POWER_RANGERS + "/";
+            public const string DISCO_CLIPS_PATH = SOUNDS_PATH + DISCO + "/";
+
+            public const string PIKACHU = "Pikachu";
+            public const string PAMELA = "Pamela";
         }
+
+        
+    }
+
+    public enum RoomSound
+    {
+        POKEMON,
+        BAY_WATCH,
+        POWER_RANGERS,
+        DISCO
+    }
+
+    public enum Bosses
+    {
+        PIKACHU,
+        PAMELA
     }
 }
