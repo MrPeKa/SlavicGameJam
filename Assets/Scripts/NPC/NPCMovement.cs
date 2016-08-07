d6cd81997ffe2c61515cb732807db3a1dee50d51
MrZacky <lukasz.slowik01@gmail.com>
2016-08-07 11:24:13 +0200

    Repaired NPC Movement and Pamela;

--- Assets/Scripts/NPC/NPCMovement.cs
+++ Assets/Scripts/NPC/NPCMovement.cs
@@ -301,8 +301,13 @@ namespace Assets.Scripts.NPC
 
         private void CheckDirectionInTargeting()
         {
+            if (SelfAnimating)
+            {
+                return;
+            }
+
             var direct = transform.position - TargetToAttack.transform.position;
-            if (!OnlyMirrorRotating)
+            if (!OnlyMirrorRotating && !SelfAnimating)
             {
                 if (Mathf.Abs(direct.x) - Mathf.Abs(direct.y) > 0)
                 {
@@ -316,7 +321,7 @@ namespace Assets.Scripts.NPC
                 else
                     FlipDown();
             }
-            else
+            else if (OnlyMirrorRotating)
             {
                 if (direct.x < 0)
                 {
