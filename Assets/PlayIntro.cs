ac58a13a6c946d2bf565eadc5a5d72f1afe51be7
Youenn-Bouglouan <youenn.bouglouan@gmail.com>
2016-08-07 11:54:40 +0200

    created menu

---
+++ Assets/PlayIntro.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+	public class PlayIntro : MonoBehaviour
+	{
+        public AudioClip Intro;
+
+		// Use this for initialization
+		void Start()
+		{
+			var audioSource = GetComponent<AudioSource>();
+			audioSource.clip = Intro;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
